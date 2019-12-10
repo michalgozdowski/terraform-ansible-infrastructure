e7f743edc2d4f1f5764e260566ca4fbfa17dd4a7
michalgozdowski <michalgozdowski@gmail.com>
2019-12-10 20:13:45 +0100

    Added key_pair resource

--- app/main.tf
+++ app/main.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
 resource "aws_instance" "webA" {
     ami = "${lookup(var.aws_ubuntu_awis,var.region)}"
     instance_type = "t2.micro"
@@ -5,7 +14,7 @@ resource "aws_instance" "webA" {
         Name = "webA-${var.name}"
         sshUser = "ubuntu"
     }
-    subnet_id = "${aws_subnet.pub-web-az-a.id}"
+    subnet_id = "${aws_subnet.web1SUBNET.id}"
     key_name = "${aws_key_pair.keypair.key_name}"
     vpc_security_group_ids = ["${aws_security_group.WebserverSG.id}"]
 }
@@ -16,7 +25,7 @@ resource "aws_instance" "webB" {
         Name = "webB-${var.name}"
         sshUser = "ubuntu"
     }
-    subnet_id = "${aws_subnet.pub-web-az-b.id}"
+    subnet_id = "${aws_subnet.web2SUBNET.id}"
     key_name = "${aws_key_pair.keypair.key_name}"
     vpc_security_group_ids = ["${aws_security_group.WebserverSG.id}"]
 }
@@ -27,7 +36,7 @@ resource "aws_instance" "bastionA" {
         Name = "bastionA-${var.name}"
         sshUser = "ubuntu"
     }
-    subnet_id = "${aws_subnet.pub-web-az-a.id}"
+    subnet_id = "${aws_subnet.web1SUBNET.id}"
     key_name = "${aws_key_pair.keypair.key_name}"
     vpc_security_group_ids = ["${aws_security_group.bastionhostSG.id}"]
 }
@@ -39,7 +48,7 @@ resource "aws_instance" "bastoionB" {
         Name = "bastionB-${var.name}"
         sshUser = "ubuntu"
     }
-    subnet_id = "${aws_subnet.pub-web-az-b.id}"
+    subnet_id = "${aws_subnet.web2SUBNET.id}"
     key_name = "${aws_key_pair.keypair.key_name}"
     vpc_security_group_ids = ["${aws_security_group.bastionhostSG.id}"]
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_vpc" "testVPC" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
