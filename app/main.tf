74f3cd10c06046cd5a743a6a18379e54aa51032a
misiek <misiek@misiek-Virtualbox.com>
2020-01-06 19:12:12 +0100

    Changes in app/ security groups, added db/ output and variable files

--- app/main.tf
+++ app/main.tf
@@ -15,6 +15,9 @@ data "terraform_remote_state" "vpc" {
    region = "eu-central-1"
  }
 }
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
 
 provider "aws" {
   region = "eu-central-1"
@@ -47,29 +50,6 @@ resource "aws_instance" "webB" {
     key_name = aws_key_pair.keypair.key_name
     vpc_security_group_ids = [aws_security_group.WEBsecuritygroup.id]
 }
-resource "aws_instance" "bastionA" {
-    ami = "ami-0cc0a36f626a4fdf5"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "bastionA-${var.name}"
-        sshUser = "ubuntu"
-    }
-    subnet_id = data.terraform_remote_state.vpc.outputs.aws_subnet_web1_id
-    key_name = aws_key_pair.keypair.key_name
-    vpc_security_group_ids = [aws_security_group.BASsecuritygroup.id]
-}
-
-resource "aws_instance" "bastoionB" {
-    ami = "ami-0cc0a36f626a4fdf5"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "bastionB-${var.name}"
-        sshUser = "ubuntu"
-    }
-    subnet_id = data.terraform_remote_state.vpc.outputs.aws_subnet_web2_id
-    key_name = aws_key_pair.keypair.key_name
-    vpc_security_group_ids = [aws_security_group.BASsecuritygroup.id]
-}
 
 resource "aws_elb" "lb" {
     name_prefix = "${var.name}lb"
@@ -86,6 +66,18 @@ resource "aws_elb" "lb" {
         instance_protocol = "http"
         lb_port = 80
         lb_protocol = "http"
+    }
+    listener {
+        instance_port = 443
+        instance_protocol = "https"
+        lb_port = 443
+        lb_protocol = "https"
+    }
+    listener {
+        instance_port = 22
+        instance_protocol = "tcp"
+        lb_port = 22
+        lb_protocol = "tcp"
     }
     cross_zone_load_balancing = true
     instances = [aws_instance.webA.id, aws_instance.webB.id]
@@ -110,25 +102,18 @@ resource "aws_security_group" "LBsecuritygroup" {
         cidr_blocks = ["0.0.0.0/0"]
         description = "Allow incoming HTTPS traffic from anywhere"
     }
-
-    egress {
-        from_port = 80
-        to_port = 80
+    ingress {
+        from_port = 22
+        to_port = 22
         protocol = "TCP"
-        security_groups = ["${aws_security_group.WEBsecuritygroup.id}"]
+        cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+        description = "Allow incoming SSH traffic from my IP only"
     }
-
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        security_groups = ["${aws_security_group.WEBsecuritygroup.id}"]
-    }
-
     tags = {
         Name = "LB-securitygroup"
     }
 }
+
 resource "aws_security_group" "WEBsecuritygroup" {
     name = "WEBsecuritygroup"
     vpc_id = data.terraform_remote_state.vpc.outputs.aws_vpc_id
@@ -137,91 +122,76 @@ resource "aws_security_group" "WEBsecuritygroup" {
         from_port = 22
         to_port = 22
         protocol = "TCP"
-        security_groups = ["${aws_security_group.BASsecuritygroup.id}"]
-        description = "Allow incoming SSH traffic from Bastion Host"
+        security_groups = ["${aws_security_group.LBsecuritygroup.id}"]
+        description = "Allow incoming SSH traffic from Load Balancer"
     }
-  ingress {
+    ingress {
       from_port = -1
       to_port = -1
       protocol = "ICMP"
-      security_groups = ["${aws_security_group.BASsecuritygroup.id}"]
+      security_groups = ["${aws_security_group.LBsecuritygroup.id}"]
       description = "Allow incoming ICMP from management IPs"
-  }
+    }
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
         self = true
     }
-    egress {
-        from_port = 3128
-        to_port = 3128
-        protocol = "TCP"
-        security_groups = ["${aws_security_group.BASsecuritygroup.id}"]
-    }
     tags = {
         Name = "WebServer-securitygroup"
     }
 }
 
-resource "aws_security_group" "BASsecuritygroup" {
-  name = "BASsecuritygroup"
-  vpc_id = data.terraform_remote_state.vpc.outputs.aws_vpc_id
-  description = "Security group for bastion hosts"
-  ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "TCP"
-      cidr_blocks = var.mgmt_ips
-      description = "Allow incoming SSH from management IPs"
-  }
+resource "aws_security_group_rule" "allowHTTP" {
+  type            = "egress"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  security_group_id = aws_security_group.LBsecuritygroup.id
 
-  ingress {
-      from_port = -1
-      to_port = -1
-      protocol = "ICMP"
-      cidr_blocks = var.mgmt_ips
-      description = "Allow incoming ICMP from management IPs"
-  }
-  egress {
-      from_port = 0
-      to_port = 0
-      cidr_blocks = ["0.0.0.0/0"]
-      protocol = "-1"
-      description = "Allow all outgoing traffic"
-  }
-  tags = {
-      Name = "Bastion-securitygroup"
-  }
 }
 
-resource "aws_security_group_rule" "LBaccess" {
-    security_group_id = "${aws_security_group.WEBsecuritygroup.id}"
-    type = "ingress"
-    from_port = 80
-    to_port = 80
-    protocol = "TCP"
-    source_security_group_id = "${aws_security_group.LBsecuritygroup.id}"
-    description = "Allow Squid proxy access from loadbalancers"
+resource "aws_security_group_rule" "allowHTTPS" {
+  type            = "egress"
+  from_port       = 443
+  to_port         = 443
+  protocol        = "tcp"
+  security_group_id = aws_security_group.LBsecuritygroup.id
 }
 
-resource "aws_security_group_rule" "LBaccess2" {
-    security_group_id = "${aws_security_group.WEBsecuritygroup.id}"
-    type = "ingress"
-    from_port = 443
-    to_port = 443
-    protocol = "TCP"
-    source_security_group_id = "${aws_security_group.LBsecuritygroup.id}"
-    description = "Allow Squid proxy access from loadbalancers"
+resource "aws_security_group_rule" "allowSSH" {
+  type            = "egress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  security_group_id = aws_security_group.LBsecuritygroup.id
+
 }
 
-resource "aws_security_group_rule" "PROXYaccess" {
-    security_group_id = "${aws_security_group.BASsecuritygroup.id}"
-    type = "ingress"
-    from_port = 3128
-    to_port = 3128
-    protocol = "TCP"
-    source_security_group_id = "${aws_security_group.WEBsecuritygroup.id}"
-    description = "Allow Squid proxy access from webservers"
+resource "aws_security_group_rule" "WEBallowSSH" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  security_group_id = aws_security_group.WEBsecuritygroup.id
+  source_security_group_id = aws_security_group.LBsecuritygroup.id
 }
 
+resource "aws_security_group_rule" "WEBallowHTTP" {
+  type            = "ingress"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  security_group_id = aws_security_group.WEBsecuritygroup.id
+  source_security_group_id = aws_security_group.LBsecuritygroup.id
+}
+
+resource "aws_security_group_rule" "WEBallowHTTPS" {
+  type            = "ingress"
+  from_port       = 443
+  to_port         = 443
+  protocol        = "tcp"
+  security_group_id = aws_security_group.WEBsecuritygroup.id
+  source_security_group_id = aws_security_group.LBsecuritygroup.id
+}
